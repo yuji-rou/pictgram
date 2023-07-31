f293173a0931a7b40788d66495a95f0e7289f7dd
yujirou <yujirou@example.com>
2023-07-31 16:26:46 +0900

    お気に入り画面の作成を更新 #9

--- src/main/java/com/example/pictgram/controller/SessionsController.java
+++ src/main/java/com/example/pictgram/controller/SessionsController.java
@@ -4,9 +4,15 @@ import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 
 @Controller
 public class SessionsController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/login")
 	public String index() {
@@ -14,20 +20,21 @@ public class SessionsController {
 	}
 
 	@GetMapping(path = "/login-failure")
-	public String loginFailure(Model model) {
+	public String loginFailure(Model model, Locale locale) {
 		model.addAttribute("hasMessage", true);
 		model.addAttribute("class", "alert-danger");
-		model.addAttribute("message", "Emailまたはパスワードに誤りがあります。");
+		model.addAttribute("message", messageSource.getMessage("sessions.loginFailure.flash", new String[] {}, locale));
 
 		return "sessions/new";
 	}
 
 	@GetMapping(path = "/logout-complete")
-	public String logoutComplete(Model model) {
+	public String logoutComplete(Model model, Locale locale) {
 		System.out.println("ログアウト");
 		model.addAttribute("hasMessage", true);
 		model.addAttribute("class", "alert-info");
-		model.addAttribute("message", "ログアウトしました。");
+		model.addAttribute("message",
+				messageSource.getMessage("sessions.logoutComplete.flash", new String[] {}, locale));
 
 		return "layouts/complete";
 	}
